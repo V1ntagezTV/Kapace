6e18ad7065885b5833d6b5bc4d1f4955317f3d98
1nsurka <insur.kunafin@yandex.ru>
2023-03-08 15:26:06 +0300

    add bordered background

--- src/components/Base/BaseBackground.vue
+++ src/components/Base/BaseBackground.vue
@@ -6,10 +6,13 @@
   <div v-else-if="props.type === 2" class="base-background__secondary">
     <slot />
   </div>
+
+  <div v-else-if="props.type === 3" class="base-background__bordered">
+    <slot />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue';
 
 const props = defineProps({
   type: {type: Number, default: 1, required: false}
@@ -19,10 +22,10 @@ const props = defineProps({
 
 <style lang="scss">
 .base-background__default {
-    background: #FFFFFF;
-    border-radius: 4px;
-    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 16%);
-    overflow: hidden;
+  background: #FFFFFF;
+  border-radius: 4px;
+  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 16%);
+  overflow: hidden;
 }
 
 .base-background__secondary {
@@ -31,6 +34,10 @@ const props = defineProps({
   overflow: hidden;
 }
 
-
-
+.base-background__bordered {
+  background: #FFFFFF;
+  border-radius: 6px;
+  border: 1px solid var(--font-gray-v1);
+  overflow: hidden;
+}
 </style>
