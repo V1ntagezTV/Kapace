519802d7c889e5ca2c18d6c4ab6bc7df91c90905
1nsurka <insur.kunafin@yandex.ru>
2024-09-07 21:45:16 +0300

    [search-view] update to new async-search-selector

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="search-page__container">
+  <div class="material search-page__container">
     <div class="search-page__body">
-      <SearchComponent
-        :input-text="searchInput"
-        @change:input-change="EmitInputChange"
+      <async-search-selector
+        v-model="searchInput"
+        class="search-page__search m3-bg-1 m-radius-1"
+        :values="searchSelectableValues.map(value => value.Title)"
+        :placeholder="'Поиск по сайту'"
+        @change:input="searchBy"
         @keyup.enter="PressSearchEnter"
       />
       <SearchTags
@@ -32,12 +35,11 @@
 </template>
 
 <script lang="ts" setup>
-import SearchComponent from '@/components/Body/Search/SearchComponent.vue';
 import SearchTags from '@/components/Body/Search/SearchTags.vue';
 import SearchContent from '@/components/Body/Search/SearchContent.vue';
 import SearchFilters from '@/components/Search/Filter/SearchFilters.vue';
 import {inject, onMounted, ref} from "vue";
-import {ContentService} from "@/api/ContentService";
+import {ContentService, V1SearchByResponseUnit} from "@/api/ContentService";
 import {V1GetByQueryRequest, V1GetByQuerySearchFilters} from "@/api/Requests/V1GetByQueryRequest";
 import {V1GetByQueryResponse} from "@/api/Requests/V1GetByQueryResponse";
 import {ContentSelectedInfo} from "@/api/Enums/ContentSelectedInfo";
@@ -46,6 +48,7 @@ import {ContentStatus} from "@/api/Enums/ContentStatus";
 import {Country} from "@/api/Enums/Country";
 import {FilterTypes} from "@/components/Search/Models/FilterTypes";
 import FilterUnitModel from "@/components/Search/Models/FilterUnitModel";
+import AsyncSearchSelector from "@/components/Base/Selector/AsyncSearchSelector.vue";
 
 const contentService: ContentService = inject("content-service")
 
@@ -71,15 +74,26 @@ const filtersV2 = ref<FilterUnitModel[]>([
 	new FilterUnitModel('Статус', FilterTypes.ContentStatus as typeof FilterTypes, statusFilters),
 	new FilterUnitModel('Страна', FilterTypes.Country as typeof FilterTypes, countryFilters)
 ]);
-const searchInput = ref("");
+const searchInput = ref<string>("");
+const searchSelectableValues = ref<V1SearchByResponseUnit[]>([])
 const initPageContent = ref<V1GetByQueryResponse>(null);
 const isDataReady = ref(false);
 const watchableShowFilters = ref(false);
 
 onMounted(async () => {
+  isDataReady.value = false;
   initPageContent.value = await GetContentAsync();
   isDataReady.value = true;
 })
+
+async function searchBy(newInput: string, isSelected: boolean) {
+  const response = await contentService.searchBy(newInput);
+  searchSelectableValues.value = response.Units;
+
+  if (isSelected) {
+    initPageContent.value = await GetContentAsync();
+  }
+}
 
 async function GetContentAsync() : Promise<V1GetByQueryResponse> {
 	const request = new V1GetByQueryRequest();
@@ -125,11 +139,7 @@ function EmitCleanFilters() {
 }
 
 function EmitDeleteSingleFilter(filterType: typeof FilterTypes, tag: string) {
-	console.log("loh: "+tag)
-}
-
-function EmitInputChange(newValue: string) {
-  searchInput.value = newValue;
+  console.log("loh: " + tag)
 }
 
 async function PressSearchEnter() {
@@ -138,8 +148,26 @@ async function PressSearchEnter() {
 </script>
 
 <style lang="scss">
-
 .search-page {
+  &__search {
+    background: var(--surface-container-lowest100);
+    border: 1px solid var(--outline-variant-light);
+    transition:
+      background-color 0.2s,
+      border-top-color 0.15s,
+      border-bottom-color 0.15s,
+      border-right-color 0.15s,
+      border-left-color 0.15s;
+
+    &:hover {
+      border: 1px solid var(--primary40);
+    }
+
+    &:focus-within {
+      border: 1px solid var(--secondary);
+    }
+  }
+
   &__container {
     padding: 20px 0;
     display: flex;
